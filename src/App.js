0885234b22b2c5858abc23bd0be05d51b9fd5b57
1sosed9 <vladsosed63@gmail.com>
2023-06-21 22:37:31 +0300

    delete two tasks

--- src/App.js
+++ src/App.js
@@ -24,16 +24,7 @@ function App() {
             title: 'third todo',
             status: false
         },
-        {
-            id: 4,
-            title: 'Проверка автоматической сборки',
-            status: false
-        },
-        {
-            id: 5,
-            title: 'Добавляем пятую задачу',
-            status: false
-        },
+       
     ])
 
     return (
